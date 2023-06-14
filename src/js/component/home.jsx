5b995fe7cb92f9480a0db727c23746fd8c0d76f0
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-06-14 11:18:09 +0000

    add code for navbar.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../component/card.jsx"
+import Navbar from "../component/navbar.jsx"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,15 +8,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<div className="d-flex justify-content-between">
+		
+		<div>
+			<div><Navbar/> </div>
+			<div>Jumbotron</div>
+			<div className="text-center">
+				<div className="d-flex justify-content-between">
 				<Card/>
 				<Card/>
 				<Card/>
 				<Card/>
+				</div>
+
 			</div>
+			<div>Footer</div>
 
 		</div>
+
+		
 	);
 };
 

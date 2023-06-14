078342fd42db0fb4ed04c3732a27f5b3d03f69ec
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-06-14 11:24:37 +0000

    add code for jumbotron.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../component/card.jsx"
 import Navbar from "../component/navbar.jsx"
+import Jumbotron from "../component/jumbotron.jsx"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -11,7 +12,7 @@ const Home = () => {
 		
 		<div>
 			<div><Navbar/> </div>
-			<div>Jumbotron</div>
+			<div> <Jumbotron/> </div>
 			<div className="text-center">
 				<div className="d-flex justify-content-between">
 				<Card/>
---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+const Jumbotron = () => {
+    return (
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Fluid jumbotron</h1>
+    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+    <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+  </div>
+</div>
+    )
+};
+
+export default Jumbotron;

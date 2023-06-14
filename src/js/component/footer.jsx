f421403c2793329927aa0c935e72252cadf4d50a
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-06-14 11:33:38 +0000

    add code for footer.jsx

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Footer = () => {
+    return (
+    <div className="bg-dark text-center text-white">
+        <p> Copyright MyWebsite 2023</p>
+    </div>
+    )
+};
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "../component/card.jsx"
 import Navbar from "../component/navbar.jsx"
 import Jumbotron from "../component/jumbotron.jsx"
+import Footer from "../component/footer.jsx"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -22,7 +23,7 @@ const Home = () => {
 				</div>
 
 			</div>
-			<div>Footer</div>
+			<div> <Footer/> </div>
 
 		</div>
 

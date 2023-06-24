d9493dcb56028f94763864ed4958b5c47298e0c0
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-06-24 23:39:30 +0000

    add props to card

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../../styles/index.css"
 
-const Card = () => {
+const Card = (props) => {
     return (
-    <div className="card cardworker p-2">
+    <div className="card cardworker p-2 m-3">
         <img src="http://www.barleymowhorsley.co.uk/img/office/our-office-8.jpg" className="card-img-top" alt="..."/>
         <div className="card-body">
             <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p className="card-text">{props.cardText}</p>
         </div>
         <div className="card-footer">
             <small className="text-muted">Last updated 3 mins ago</small>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -15,11 +15,11 @@ const Home = () => {
 			<div><Navbar/> </div>
 			<div> <Jumbotron/> </div>
 			<div className="text-center">
-				<div className="d-flex justify-content-between">
-				<Card/>
-				<Card/>
-				<Card/>
-				<Card/>
+				<div className="d-flex justify-content-center">
+				<Card cardText="Content A: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
+				<Card cardText="Content B:Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."/>
+				<Card cardText="Content C:Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
+				<Card cardText="Content D:Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."/>
 				</div>
 
 			</div>
